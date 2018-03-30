4351702b0308d98953b482eb95b71763ba3e646d
Maxim Tretyakov <max@tretyakov-ma.ru>
2018-03-30 22:34:41 +0500

    imgs fix

--- imgs.sh
+++ imgs.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-find -name '*.mp4' -exec ffmpeg -ss 00:00:01 -i {} -vframes 1 -q:v 2 {}.png \;
+mkdir -p ./imgs
+find -type f \( -name '*.mp4' -or -name '*.MP4' \) \
+     -exec ffmpeg -ss 00:00:01 -i {} -vframes 1 -q:v 2 imgs/{}.origin.png \; \
+     -exec convert imgs/{}.origin.png -resize 640 imgs/{}.png \; \
+     -exec rm imgs/{}.origin.png \;
