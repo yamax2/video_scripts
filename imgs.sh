a21cdb62868405719f7925ea9095992166e76c25
Maxim Tretyakov <max@tretyakov-ma.ru>
2021-05-23 12:05:48 +0500

    fixes

--- imgs.sh
+++ imgs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 mkdir -p ./imgs
 find -type f \( -name '*.mp4' -or -name '*.MP4' \) \
      -exec ffmpeg -ss 00:00:01 -i {} -vframes 1 -q:v 2 imgs/{}.origin.png \; \
--- join.sh
+++ join.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-find -name '*.MP4' -printf "file '%f'\n" | sort > mylist.txt && \
-ffmpeg -f concat -i mylist.txt -c:s copy -c:v libx264 -b:v 13000k -map 0:0 -map 0:2 output.mp4
+set -e
+
+find -name '*.MP4' -printf "file '%f'\n" | sort > mylist.txt
+ffmpeg -f concat -i mylist.txt -c:s copy -c:v libx264 -b:v 13000k -map 0:0 -map 0:2 "${PWD##*/}.mp4"
+rm mylist.txt
